b1b980336e480630d46beb443bbe57b74e15f166
AstaDk <asta.dev.520@gmail.com>
2023-05-30 22:04:36 +0700

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const port = 8000;
+
+const app: express.Express = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("HELLO FROM EXPRESS + TS!!!!");
+});
+
+app.listen(port, () => {
+  console.log(`now listening on port ${port}`);
+});
