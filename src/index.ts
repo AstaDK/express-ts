43cd76a6656dd905507d40c485f733c43ede332e
AstaDk <asta.dev.520@gmail.com>
2023-05-30 23:05:01 +0700

    feat: config app dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-import express from 'express';
-const port = 8000;
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+dotenv.config();
 
-const app: express.Express = express();
+const app: Express = express();
+app.use(cors());
+app.use(helmet({ crossOriginResourcePolicy: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms - [:date[clf]] - ":user-agent"'
+  )
+);
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('HELLO FROM EXPRESS + TS!!!!');
+const port = process.env.PORT;
+
+app.get('/', (_, res: Response) => {
+  res.send('Welcome API');
 });
 
 app.listen(port, () => {
-  console.log(`now listening on port ${port}`);
+  console.log(`🚀 [Server] is running at http://localhost:${port}`);
 });
